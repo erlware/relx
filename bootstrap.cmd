91481adf8d5209f2bf99118c8ff9fd78d1670671
nuex <nx@nu-ex.com>
2014-02-21 15:16:46 -0500

    Windows support

---
+++ bootstrap.cmd
@@ -0,0 +1,12 @@
+:: A script to build relx on Windows
+:: Requires rebar
+
+:: Get dependencies, compile and escriptize relx
+@cmd /c @rebar -r get-deps compile escriptize
+
+:: Create a shortcut file for running the relx command
+@set relx_cmd=relx.cmd
+@echo @echo off > %relx_cmd%
+@echo setlocal >> %relx_cmd%
+@echo set relx=%%~f0 >> %relx_cmd%
+@echo escript ^"%%relx:.cmd=%%^" %%* >> %relx_cmd%
