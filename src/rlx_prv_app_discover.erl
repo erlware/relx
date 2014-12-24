e6344b033ddad5681b04e8492df019de5ef92a50
Tristan Sloughter <t@crashfast.com>
2014-12-24 14:15:48 -0600

    only unique lib dirs should be used to save time/confusion

--- src/rlx_prv_app_discover.erl
+++ src/rlx_prv_app_discover.erl
@@ -55,7 +55,7 @@ init(State) ->
 %% looking for OTP Applications
 -spec do(rlx_state:t()) -> {ok, rlx_state:t()} | relx:error().
 do(State0) ->
-    LibDirs = get_lib_dirs(State0),
+    LibDirs = lists:usort(get_lib_dirs(State0)),
     case rlx_app_discovery:do(State0, LibDirs) of
         {ok, AppMeta} ->
             State1 = rlx_state:available_apps(State0, AppMeta),
