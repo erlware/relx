7bd73e22deb51d7e976215adf2ef403bf42ce26b
Heinz N. Gies <heinz@licenser.net>
2015-11-01 00:51:16 +0100

    Switch to low color mode for logs

--- src/rlx_state.erl
+++ src/rlx_state.erl
@@ -146,7 +146,7 @@ new(Config, CommandLineConfig, Targets)
     {ok, Root} = file:get_cwd(),
 
     Caller = proplists:get_value(caller, CommandLineConfig, api),
-    Log = proplists:get_value(log, CommandLineConfig, ec_cmd_log:new(error, Caller)),
+    Log = proplists:get_value(log, CommandLineConfig, ec_cmd_log:new(error, Caller, low)),
     State0 = #state_t{log=Log,
                       config_file=Config,
                       cli_args=CommandLineConfig,
