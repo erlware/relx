de523c2d3920d01493f08a0d69ab99c74906c8bf
Tristan Sloughter <t@crashfast.com>
2020-04-21 09:30:27 -0600

    use github actions gleam-lang/setup-erlang to run shell tests

--- shelltests/run_tests.sh
+++ shelltests/run_tests.sh
@@ -8,4 +8,18 @@ export TERM=dumb
 
 find . -name _build | xargs rm -rf
 
-shelltest -c --diff --all --debug --execdir */*.test
+current_dir=$(pwd)
+rebar3_dir=$(mktemp -d)
+
+pushd "${rebar3_dir}"
+
+# clone latest rebar3 and build with relx as a checkout
+git clone https://github.com/erlang/rebar3 .
+mkdir _checkouts
+ln -s "$current_dir/../../relx" _checkouts/relx
+sed -i 's_relx\(.*\)build/default/lib/_relx\1checkouts_' rebar.config
+./bootstrap
+
+popd
+
+PATH="${rebar3_dir}":$PATH ~/.cabal/bin/shelltest -c --diff --all --debug --execdir -- */*.test
