09fa41e4f3eb6175274fde246b759b3495da9ee9
Regan Heath <reganmheath@gmail.com>
2020-05-21 01:34:29 +0100

    Pass erlpath from install to test

--- shelltests/wintest.ps1
+++ shelltests/wintest.ps1
@@ -1,20 +1,37 @@
 #! /usr/bin/pwsh
+param (
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$erlpath
+)
+
 # Test build, install, start, ping, stop, uninstall of powershell_release
 $release = "powershell_release"
 
 # Terminate on error
 $ErrorActionPreference = "Stop"
 
+# Get ERTS version
+$erts_vsn = & "$erlpath\bin\erl.exe" -boot no_dot_erlang -noshell -eval 'io:format(\"~s\", [erlang:system_info(version)]), halt().'
+
+# Add erlpath to PATH
+"*** Add to PATH $erlpath\bin"
+"*** Add to PATH $erlpath\erts-$erts_vsn\bin"
+$env:PATH = "$erlpath\erts-$erts_vsn\bin;$erlpath\bin;$env:PATH"
+""
+
 # CD to script location (shelltests)
 Set-Location $PSScriptRoot
 
 # Clean all builds (continue on error)
+"*** Clean"
 Get-ChildItem -Path . -Filter _build -Recurse | ForEach-Object { 
     "Remove $($_.FullName).."
     Remove-Item $_.FullName -Recurse -Force -ErrorAction SilentlyContinue
 }
+""
 
 # Create temporary build folder for rebar3
+"*** Build rebar3"
 $rebar3_dir = "$PSScriptRoot\$(([System.Guid]::NewGuid()).Guid)"
 mkdir $rebar3_dir | Out-Null
 
@@ -26,9 +43,10 @@ New-Item -ItemType SymbolicLink -Path "_checkouts\relx" -Target "$PSScriptRoot\.
 (Get-Content rebar.config) -replace 'relx(.*)build/default/lib/', 'relx$1checkouts' | Set-Content rebar.config -Encoding ASCII
 cmd /c bootstrap.bat
 Pop-Location
+""
 
 # Function to run rebar3
-function Rebar() { & escript.exe "$rebar3_dir\rebar3" @args }
+function Rebar() { & "$erlpath\bin\escript.exe" "$rebar3_dir\rebar3" @args }
 
 # Our release source
 Set-Location ".\$release\"
