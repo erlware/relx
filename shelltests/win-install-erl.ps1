d88534d45e9c9a8b81f41f82b5cd18f672195a1e
Regan Heath <reganmheath@gmail.com>
2020-05-21 01:34:29 +0100

    Pass erlpath from install to test

--- shelltests/win-install-erl.ps1
+++ shelltests/win-install-erl.ps1
@@ -1,5 +1,4 @@
 #! /usr/bin/pwsh
-
 param (
     [Parameter(Mandatory=$true, Position=0)]
     [int]$otp_version
@@ -13,28 +12,22 @@ $ErrorActionPreference = "Stop"
 Invoke-WebRequest -Uri "https://packages.erlang-solutions.com/erlang/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_$otp_version.0~windows_amd64.exe" -OutFile "esl-erlang_$otp_version.0~windows_amd64.exe"
 
 # Install erlang (runs in background)
+$erlroot = "$PSScriptRoot\erl"
 "Installing esl-erlang_$otp_version.0~windows_amd64.exe..."
-& ".\esl-erlang_$otp_version.0~windows_amd64.exe" '/S'
-
-# Expected installation folder varies by version
-switch($otp_version) {
-    19 { $erlroot = "$env:ProgramFiles\erl8.0" }
-    20 { $erlroot = "$env:ProgramFiles\erl9.0" }
-    21 { $erlroot = "$env:ProgramFiles\erl10.2" }
-    22 { $erlroot = "$env:ProgramFiles\erl10.4" }
-    Default { $erlroot = "$env:ProgramFiles\erl-$otp_version.0" }
-}
+& ".\esl-erlang_$otp_version.0~windows_amd64.exe" '/S' "/D=$erlroot"
 
 # Wait for it..
 while (@(Get-Process | Where-Object { $_.name -match 'esl-erlang' }).length -gt 0) {
     Start-Sleep -Milliseconds 100
 }
 
-# Update system path
-"Add $erlroot\bin to PATH"
-$oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
-$newpath = "$erlroot\bin;$oldpath"
-Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+# Verify installation
+if (! (Test-Path "$erlroot\bin\") ) { Write-Error "Erlang NOT installed in $erlroot?!" }
+"Erlang installed in $erlroot"
 
-# Cleanup
-Remove-Item "esl-erlang_$otp_version.0~windows_amd64.exe"
+# Verify tools
+if (! (Test-Path "$erlroot\bin\erl.exe") )     { Write-Error "Erlang MISSING erl.exe?!" }
+if (! (Test-Path "$erlroot\bin\escript.exe") ) { Write-Error "Erlang MISSING escript.exe?!" }
+
+# Set output of step to erlang path
+"::set-output name=erlpath::$erlroot"
