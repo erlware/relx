9432adca91e1fc6ec4975298df1759b229904a4b
Shunichi Shinohara <shino@shiguredo.jp>
2022-04-01 10:21:50 +0900

    Fix include_erts=false failure with OTP-25.0-rc2
    cf. Release fails to start when rebar3's include_erts=false is used, 25.0-rc2 · Issue #5826 · erlang/otp
        https://github.com/erlang/otp/issues/5826

--- src/rlx_assemble.erl
+++ src/rlx_assemble.erl
@@ -863,24 +863,11 @@ make_boot_script_variables(Release, State) ->
     % A boot variable is needed when {system_libs, false} and the application
     % directories are split between the release/lib directory and the erlang
     % install directory on the target host.
-    % The built-in $ROOT variable points to the erts directory on Windows
-    % (dictated by erl.ini [erlang] Rootdir=) and so a boot variable is made
-    % pointing to the release directory
-    % On non-Windows, $ROOT is set by the ROOTDIR environment variable as the
-    % release directory, so a boot variable is made pointing to the erts
-    % directory.
     % NOTE the boot variable can point to either the release/erts root directory
     % or the release/erts lib directory, as long as the usage here matches the
     % usage used in the start up scripts
-    case {os:type(), rlx_state:system_libs(State)} of
-        {{win32, _}, false} ->
-            [{"RELEASE_DIR", filename:join(rlx_state:base_output_dir(State),
-                                           rlx_release:name(Release))}];
-        {{win32, _}, true} ->
-            [];
-        _ ->
-            [{"SYSTEM_LIB_DIR", code:lib_dir()}]
-    end.
+    [{"RELEASE_DIR", filename:join(rlx_state:base_output_dir(State), rlx_release:name(Release))},
+     {"SYSTEM_LIB_DIR", code:lib_dir()}].
 
 create_no_dot_erlang(RelDir, OutputDir, Options, _State) ->
     create_other_boot_file(RelDir, [no_dot_erlang | Options], "no_dot_erlang"),
